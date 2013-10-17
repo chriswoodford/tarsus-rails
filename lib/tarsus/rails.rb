dda5d1567566b4b52a9af54d7aad2ac72cb00fe3
Chris Woodford <chris.woodford@gmail.com>
2013-10-17 12:54:04 -0400

    clean up rails file

--- lib/tarsus/rails.rb
+++ lib/tarsus/rails.rb
@@ -1,9 +1,2 @@
-require 't/rails/engine' if ::Rails.version >= '3.1'
-require 'tarsus/rails/railtie'
+require 'tarsus/rails/engine'
 require 'tarsus/rails/version'
-
-module Tarsus
-  module Rails
-
-  end
-end
--- lib/tarsus/rails/railtie.rb
+++
@@ -1,8 +0,0 @@
-# Used to ensure that Rails 3.0.x, as well as Rails >= 3.1 with asset pipeline disabled
-# get the minified version of the scripts included into the layout in production.
-module Tarsus
-  module Rails
-    class Railtie < ::Rails::Railtie
-    end
-  end
-end
